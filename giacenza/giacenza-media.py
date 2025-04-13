195baf25084e050e757e83c783182d56bb100cc9
Michele Baldessari <michele@acksyn.org>
2025-04-13 11:59:32 +0200

    more

--- giacenza/giacenza-media.py
+++ giacenza/giacenza-media.py
@@ -3,6 +3,31 @@
 import pandas as pd
 
 
+# Time series to be downloaded from
+# https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html
+euroxref = pd.read_csv("./eurofxref-hist.csv", parse_dates=["Date"])
+
+
+def get_daily_eur_gbp(day, currency="GBP"):
+    newday = day
+    while True:
+        result = euroxref[euroxref["Date"] == newday]
+        if len(result) > 1:
+            raise Exception("Result more than one line for: %s" % newday)
+        # The euroxref only contains working days so we will take the first
+        # next available exchange rate
+        if result.empty:
+            newday = newday - pd.Timedelta(days=1)
+        else:
+            break
+        if newday < pd.Timestamp("1999-01-04"):
+            raise Exception("Something very wrong happened: %s" % newday)
+
+    rate = result.iloc[0]["GBP"]
+    # print(f"Asked for day: {day} Got: {newday} -> {rate}")
+    return rate
+
+
 # This assumes that the first entry of the day is the last one
 # I.e. the CSV is sorted from newest to oldest!
 def get_daily_balance(csv_file):
@@ -14,16 +39,22 @@ def get_daily_balance(csv_file):
         day = row["Date"]
         if day not in seen_days:
             seen_days.add(day)
-            balance[day] = row["Running Balance"]
+            xchange = get_daily_eur_gbp(day)
+            euros = xchange * row["Running Balance"]
+            balance[day] = euros
         else:
             # If we seen this day we skip it (it was the first)
             continue
         count += 1
-        # print(row)
-        # print(day)
+        # print(f"GBP: {xchange}")
 
     print(f"TOTAL: {count}")
     return balance
 
 
-get_daily_balance("~/statement_37543175_GBP_2024-01-01_2024-12-31.csv")
+def get_giacenza_media(b):
+    return sum(b.values()) / len(b)
+
+
+bal = get_daily_balance("~/statement_37543175_GBP_2024-01-01_2024-12-31.csv")
+print(f"Giacenza media in EUR: {get_giacenza_media(bal)}")
